53cff9ddf62cd2c945ed0b3df37b6acd2f1c4e08
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-09-20 10:35:48 +0300

    Parallax effect in the flickity example.

--- examples/sliders/flickity-example/src/pages/index.jsx
+++ examples/sliders/flickity-example/src/pages/index.jsx
@@ -6,9 +6,11 @@ import Flickity from "flickity";
 import Layout from "@affectionatedoor/gatsby-theme-ui/src/components/Layout";
 import Centered from "@affectionatedoor/gatsby-theme-ui/src/components/Centered";
 
-// let flkty = null;
+let flkty = null;
 // const contentSelector = ".flickity-slider img";
+// const contentSelector = ".flickity-slider h2";
 // const contentSelector = ".flickity-slider > div > div";
+const contentSelector = ".flickity-slider .content";
 
 const Content = () => (
   <Fragment>
@@ -24,40 +26,66 @@ const Content = () => (
 export default () => {
   const carouselRef = useRef();
   useEffect(() => {
-    // eslint-disable-next-line no-new
-    new Flickity(".carousel");
-    // const { slides } = flkty;
-    // function scrollHandler() {
-    //   const things = carouselRef.current.querySelectorAll(contentSelector);
-    //   console.log(things);
-    //   slides.forEach(function(slide, i) {
-    //     var thing = things[i];
-    //     var x = ((slide.target + flkty.x) * -1) / 3;
-    //     thing.style.transform = `translateX(${x}px)`;
-    //     // if (i === 0) console.log(thing.style.transform);
-    //   });
-    // }
-    // flkty.on("scroll", scrollHandler);
+    flkty = new Flickity(".carousel");
+    const { slides } = flkty;
+    const things = carouselRef.current.querySelectorAll(contentSelector);
+    function scrollHandler() {
+      slides.forEach(function(slide, i) {
+        const thing = things[i];
+        const x = (slide.target + flkty.x) * 0.53;
+        thing.style.transform = `translateX(${-x}px)`;
+      });
+    }
+    flkty.on("scroll", scrollHandler);
   }, []);
   return (
     <Layout>
       <div className="carousel" ref={carouselRef}>
-        <Box sx={{ width: "100%" }}>
-          <Centered sx={{ minHeight: "500px", bg: "secondary" }}>
-            <Styled.h2>Slide #1</Styled.h2>
-            <Content />
+        <Box sx={{ width: "100%", margin: 2 }}>
+          <Centered
+            sx={{
+              p: 4,
+              minHeight: "500px",
+              bg: "secondary",
+              overflow: "hidden"
+            }}
+          >
+            <div className="content">
+              <Styled.h2>Slide #1</Styled.h2>
+              <Content />
+            </div>
           </Centered>
         </Box>
-        <Box sx={{ width: "100%" }}>
-          <Centered sx={{ minHeight: "500px", bg: "primary", color: "white" }}>
-            <Styled.h2>Slide #2</Styled.h2>
-            <Content />
+        <Box sx={{ width: "100%", margin: 2 }}>
+          <Centered
+            sx={{
+              p: 4,
+              minHeight: "500px",
+              bg: "primary",
+              color: "white",
+              overflow: "hidden"
+            }}
+          >
+            <div className="content">
+              <Styled.h2>Slide #2</Styled.h2>
+              <Content />
+            </div>
           </Centered>
         </Box>
-        <Box sx={{ width: "100%" }}>
-          <Centered sx={{ minHeight: "500px", bg: "accent", color: "white" }}>
-            <Styled.h2>Slide #3</Styled.h2>
-            <Content />
+        <Box sx={{ width: "100%", margin: 2 }}>
+          <Centered
+            sx={{
+              p: 4,
+              minHeight: "500px",
+              bg: "accent",
+              color: "white",
+              overflow: "hidden"
+            }}
+          >
+            <div className="content">
+              <Styled.h2>Slide #3</Styled.h2>
+              <Content />
+            </div>
           </Centered>
         </Box>
         {/* <div
