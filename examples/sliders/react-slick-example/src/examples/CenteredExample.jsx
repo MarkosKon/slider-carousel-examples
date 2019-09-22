6add9f0e7337c8f481191182f74ca95ef2b7c9ab
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-09-23 01:06:44 +0300

    Doing a double render hack to update the responsive centerPadding in SSR.

--- examples/sliders/react-slick-example/src/examples/CenteredExample.jsx
+++ examples/sliders/react-slick-example/src/examples/CenteredExample.jsx
@@ -1,70 +1,85 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /** @jsx jsx */
+import { useState, useEffect } from "react";
 import { jsx, Styled, Box } from "theme-ui";
 import Img from "gatsby-image";
 import PropTypes from "prop-types";
 import Slider from "react-slick";
 
-const CenteredExample = ({ images }) => (
-  <Box
-    sx={{
-      px: [1, 4, 6],
-      "& .slick-slide": {
-        transition: "all .33s ease-in-out"
-        // filter: "blur(2px)"
-      },
-      "& .slick-active": {
-        transform: "scale(1.2)"
-        // filter: "unset"
-      }
-    }}
-  >
-    <Styled.h2 sx={{ textAlign: "center" }}>Centered Example</Styled.h2>
-    <p
-      sx={{ display: "block", textAlign: "center", mb: 4, fontStyle: "italic" }}
-    >
-      It&apos;s not 100% responsive
-    </p>
-    <Slider
-      dots
-      arrows={false}
-      infinite={false}
-      speed={500}
-      centerMode
-      centerPadding="350px"
-      slidesToShow={1}
-      slidesToScroll={1}
-      responsive={[
-        {
-          breakpoint: 1500,
-          settings: { centerPadding: "150px" }
+const CenteredExample = ({ images }) => {
+  // Double render hack to update the responsive centerPadding in SSR.
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(typeof window !== "undefined");
+  }, []);
+  return (
+    <Box
+      sx={{
+        px: [1, 4, 6],
+        "& .slick-slide": {
+          transition: "all .33s ease-in-out"
+          // filter: "blur(2px)"
         },
-        {
-          breakpoint: 1200,
-          settings: { centerPadding: "50px" }
-        },
-        {
-          breakpoint: 600,
-          settings: { centerPadding: "0px" }
+        "& .slick-active": {
+          transform: "scale(1.2)"
+          // filter: "unset"
         }
-      ]}
+      }}
     >
-      {images.map(({ fluid }, i) => {
-        const slideNo = i + 1;
-        return (
-          <Box key={fluid.src} sx={{ p: 5 }}>
-            <Img
-              fluid={fluid}
-              title={`Slide ${slideNo}`}
-              alt={`Slide ${slideNo}`}
-              sx={{ height: ["400px", "600px"] }}
-            />
-          </Box>
-        );
-      })}
-    </Slider>
-  </Box>
-);
+      <Styled.h2 sx={{ textAlign: "center" }}>Centered Example</Styled.h2>
+      <p
+        sx={{
+          display: "block",
+          textAlign: "center",
+          mb: 4,
+          fontStyle: "italic"
+        }}
+      >
+        It&apos;s not 100% responsive
+      </p>
+      <Slider
+        // double render hack.
+        key={isClient}
+        dots
+        arrows={false}
+        infinite={false}
+        speed={500}
+        centerMode
+        centerPadding="350px"
+        slidesToShow={1}
+        slidesToScroll={1}
+        responsive={[
+          {
+            breakpoint: 1500,
+            settings: { centerPadding: "150px" }
+          },
+          {
+            breakpoint: 1200,
+            settings: { centerPadding: "50px" }
+          },
+          {
+            breakpoint: 600,
+            settings: { centerPadding: "0px" }
+          }
+        ]}
+      >
+        {images.map(({ fluid }, i) => {
+          const slideNo = i + 1;
+          return (
+            <Box key={fluid.src} sx={{ p: 5 }}>
+              <Img
+                fluid={fluid}
+                title={`Slide ${slideNo}`}
+                alt={`Slide ${slideNo}`}
+                sx={{ height: ["400px", "600px"] }}
+              />
+            </Box>
+          );
+        })}
+      </Slider>
+    </Box>
+  );
+};
 
 CenteredExample.propTypes = {
   images: PropTypes.arrayOf(
